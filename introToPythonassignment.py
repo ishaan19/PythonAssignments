88e5bad959ce8f00f0d8d92416965bb880964e17
ishaankapoor <ishaankapoor1906@gmail.com>
2018-06-21 21:31:21 +0530

    assigment2

---
+++ introToPythonassignment.py
@@ -0,0 +1,40 @@
+#1
+print("Hello World")
+
+
+
+#2
+a="acad"
+b="view"
+string=" "+" "
+print (string,a,b)
+
+
+
+#3
+x=int(input("enter 1st number"))
+y=input("enter name")
+z=input("enter course")
+print("number entered",x)
+print("name entered",y)
+print("course entered",z)
+
+
+
+#4
+print("let's get started")
+
+
+
+#5
+s="acadview"
+course="python"
+fees=5000
+print("join %s for %s in %d"%(s,course,fees))
+
+
+#6
+Let’s do some interesting exercise:
+name=”Tony Stark”
+salary=1000000
+print(‘%s’’%d’)%(name,salary) 
