678c7a4c63fb3ba24ce3b5fa6a738a9d16a42c2c
ishaankapoor <ishaankapoor1906@gmail.com>
2018-07-01 21:43:32 +0530

    webbasicsapiassignment

---
+++ WebBasicsAndAPI.py
@@ -0,0 +1,24 @@
+#q1 difference between json and xml files:
+JSON:stands for javascript object notation and is basically a syntax for storing and exchainging the data. 
+XML:stands for extended markup language. 
+JSON: is used to represent objects and is not as flexible as XML
+
+#q. Get the IP address of 
+Facebook:31.13.69.228
+Google:172.217.15.110
+
+#q. Difference between library and API:
+API: is an interface that provides communication between library and program
+Library: contains a set of fucntions etc to be used for a program.
+API: functionality of an API is exposed to be further used.
+Library: functionalities aren't exposed.
+Library:written in the same language
+API:can be writte in diferent languages
+
+#q. how http works:
+http stands for hyper text transfer protocol and it is used by web servers and web browsers to intercat with each other on Internet.
+there are mainly four kinds of requests on the internet. these are :
+GET:this is used to get the data and read the data already existing
+POST:creates data and is bascially used for example, uploading of files
+PUT:the put requests updates the existing data for eg, a user when wants to update the password for his email is will use the put request method.
+DELETE:the delete request is responsible for deleting the existing data over the web.
